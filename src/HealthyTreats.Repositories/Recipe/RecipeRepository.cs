5578985b9f1eae5001b654c4aa4c427db9862f91
Nastia Bondar <145715374+Nastiia18@users.noreply.github.com>
2024-04-09 02:14:17 +0300

    Create Repository

---
+++ src/HealthyTreats.Repositories/Recipe/RecipeRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthyTreats.Repositories.Recipe
+{
+    internal class RecipeRepository
+    {
+    }
+}
