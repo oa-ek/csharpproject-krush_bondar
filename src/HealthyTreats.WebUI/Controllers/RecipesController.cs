5f9a4690773143dcef7f2db63d54fa30771d15ea
Ira-name <iryna.krush@oa.edu.ua>
2024-04-09 02:21:25 +0300

    create-view

---
+++ src/HealthyTreats.WebUI/Controllers/RecipesController.cs
@@ -0,0 +1,35 @@
+﻿using HealthyTreats.Core.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HealthyTreats.WebUI.Controllers
+{
+    public class RecipesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        // GET: /Recipes/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /Recipes/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Recipe recipe)
+        {
+            if (ModelState.IsValid)
+            {
+                // Додайте код для збереження нового рецепту в базу даних
+                // Після успішного збереження можна перенаправити користувача на іншу сторінку
+                return RedirectToAction("Index"); // Наприклад, перенаправлення на сторінку зі списком рецептів
+            }
+            return View(recipe);
+        }
+
+        // Інші методи контролера, які можливо у вас є
+    }
+    }
+
