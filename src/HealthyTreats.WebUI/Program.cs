c9663cd9202ebc941d23acc4210741dd8699300e
Ira-name <iryna.krush@oa.edu.ua>
2024-05-09 04:12:13 +0300

    Reapply "Merge branch 'Registration-login'"
    This reverts commit 8afc3819650b9f6c0e4a316aec682b280eb56d28.

--- src/HealthyTreats.WebUI/Program.cs
+++ src/HealthyTreats.WebUI/Program.cs
@@ -12,8 +12,17 @@ builder.Services.AddDbContext<HealthyContext>(options =>
 	options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = true)
-	.AddEntityFrameworkStores<HealthyContext>();
+builder.Services.AddDefaultIdentity<User>(
+	options => {
+		options.SignIn.RequireConfirmedAccount = false;
+        options.Password.RequireDigit = false;
+        options.Password.RequireLowercase = false;
+        options.Password.RequireUppercase = false;
+        options.Password.RequireNonAlphanumeric = false;
+        options.Password.RequiredLength = 5;
+    }).AddRoles<IdentityRole<Guid>>()
+    .AddEntityFrameworkStores<HealthyContext>();
+
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddRepositories();
