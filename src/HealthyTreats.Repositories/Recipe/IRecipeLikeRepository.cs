012656baeb7c73de15ad336844fc951b59c834e7
Ira-name <iryna.krush@oa.edu.ua>
2024-06-10 22:58:47 +0300

    add-heart

---
+++ src/HealthyTreats.Repositories/Recipe/IRecipeLikeRepository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthyTreats.Repositories.Recipe
+{
+    public interface IRecipeLikeRepository
+    {
+        Task<int> GetLikesAsync(Guid recipeId);
+        Task AddLikeAsync(Guid recipeId);
+        Task RemoveLikeAsync(Guid recipeId);
+    }
+
+    public class RecipeLikeRepository : IRecipeLikeRepository
+    {
+        private readonly Dictionary<Guid, int> _likes = new Dictionary<Guid, int>();
+
+        public Task<int> GetLikesAsync(Guid recipeId)
+        {
+            _likes.TryGetValue(recipeId, out int likes);
+            return Task.FromResult(likes);
+        }
+
+        public Task AddLikeAsync(Guid recipeId)
+        {
+            if (_likes.ContainsKey(recipeId))
+            {
+                _likes[recipeId]++;
+            }
+            else
+            {
+                _likes[recipeId] = 1;
+            }
+            return Task.CompletedTask;
+        }
+
+        public Task RemoveLikeAsync(Guid recipeId)
+        {
+            if (_likes.ContainsKey(recipeId) && _likes[recipeId] > 0)
+            {
+                _likes[recipeId]--;
+            }
+            return Task.CompletedTask;
+        }
+    }
+    }
